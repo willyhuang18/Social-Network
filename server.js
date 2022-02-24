6e7c66d479ca690845bce8c65f29ed5cb31e075b
wiily <willyhuang18@gmail.com>
2022-02-23 21:20:40 -0800

    adding component in the server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+// Require express and mongoose
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => console.log(`Connected on http://localhost:${PORT} successfully `));
