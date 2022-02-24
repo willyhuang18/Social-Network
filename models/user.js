c6b3fd3e80c548d66ce9c5ceadf9a29e314694e8
wiily <willyhuang18@gmail.com>
2022-02-23 21:47:31 -0800

    indicate usersSchema

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const usersSchema = new Schema(
+    {
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        // validate email
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'thoughts'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    }]
+    },
+    {
+    toJSON: {
+        virtuals: true,
+        getters: true,
+    },
+    id: false
+    }
+)
