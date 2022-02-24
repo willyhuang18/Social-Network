c47e11644c92a0701ee9a924af0ec3abacd4e712
wiily <willyhuang18@gmail.com>
2022-02-23 21:33:27 -0800

    indicate the reactionSchema

---
+++ models/thoughts.js
@@ -0,0 +1,29 @@
+const { Schema, model, Types } = require('mongoose');
+
+
+const reactionSchema = new Schema(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+      },
+      responseBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+    {
+      toJSON: {
+        getters: true,
+      }
+    }
+)
