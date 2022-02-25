d7c2e3da90d4b1c8831f81903115d88f162f6efd
wiily <willyhuang18@gmail.com>
2022-02-24 17:01:17 -0800

    finalizing the server,js

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 // Require express and mongoose
 const express = require('express');
-const mongoose = require('mongoose');
 const db = require('./config/connection');
-
-
+const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(require(routes));
 
 db.once('open', () => {
     app.listen(PORT, () => 
