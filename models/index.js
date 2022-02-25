7e7fd5a404f2c3448d4e8dc3e235a644b7fb02af
wiily <willyhuang18@gmail.com>
2022-02-24 19:25:13 -0800

    change the users

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // import Users and Thoughts Models
 const Thoughts = require('./thoughts');
-const Users = require('./users');
+const Users = require('./user');
 
 
 module.exports = { Users, Thoughts };
