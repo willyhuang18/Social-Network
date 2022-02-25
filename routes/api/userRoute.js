68a0561844c025582fce39031917d1b06c4a94ba
wiily <willyhuang18@gmail.com>
2022-02-24 16:47:25 -0800

    //indicate the get and post route for user by ID

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -14,4 +14,9 @@ const {
 //indicate the get and post route for user
 router.route('/').get(getAllUsers).post(createUsers);
 
+//indicate the get and post route for user by ID
+router.route('/:id').get(getUsersById).put(updateUsers).delete(deleteUsers);
+
+//indicate the get and post route for user's friend by ID
+router.route('/:id/friends/:friendId').post(addFriend).delete(deleteFriend)
 module.exports = router; 
